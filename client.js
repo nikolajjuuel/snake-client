1ab19c240e57ae8aabcfaba734da103acd61dd55
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-22 23:33:13 +0000

    exported clint code to clinet.js and imported to play.js

--- client.js
+++ client.js
@@ -1,1 +1,28 @@
-const connect = require('./play');
+const net = require("net");
+const { IP, PORT } = require("./constants");
+
+const connect = function () {
+    const conn = net.createConnection({
+        host: IP,
+        port: PORT
+    });
+
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+
+    conn.on("data", (data) => {
+        console.log('server says',data);
+    })
+
+    conn.on('connect', () => {
+        console.log('connected to server');
+        conn.write('Name: NJJ')
+
+      });
+
+    return conn;
+};
+
+module.exports = {
+    connect,
+}
--- play.js
+++ play.js
@@ -1,32 +1,5 @@
-const net = require("net");
 const  {setupInput} = require("./input");
+const {connect} = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-    const conn = net.createConnection({
-        host: '135.23.223.133',
-        port: '50542'
-    });
-
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-
-    conn.on("data", (data) => {
-        console.log('server says',data);
-    })
-
-    conn.on('connect', () => {
-        console.log('connected to server');
-        conn.write('Name: SUP')
-
-      });
-
-    return conn;
-};
 setupInput(connect());
 
-//console.log(setupInput);
-
-console.log("Connecting ...");
-//connect();
-module.exports = connect;
