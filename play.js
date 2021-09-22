a30614c35cffee3cb32247b19f31277aa7e9ef79
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-09-22 22:06:41 +0000

    ctn c to terminate

--- play.js
+++ play.js
@@ -2,20 +2,53 @@ const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542'
-  });
+    const conn = net.createConnection({
+        host: '135.23.223.133',
+        port: '50542'
+    });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
 
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-  
+    conn.on("data", (data) => {
+        console.log('server says',data);
+    })
 
-  return conn;
+    conn.on('connect', () => {
+        console.log('connected to server');
+        conn.write('Name: SUP')
+
+        setInterval(() => {
+            conn.write('Move: left')
+        }, 100);
+
+      });
+
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+
+    stdin.on("data", handleUserInput);
+
+    return stdin;
+  };
+
+
+  const handleUserInput = function (data) {
+    // your code here
+    if (key === '\u0003') {
+       data = process.exit();
+      }
+  };
+
+
+
+    return conn;
 };
 
 console.log("Connecting ...");
